9a141a2463de5c198c413a55d29e14565d4bcf47
Tobias_Nilsson <tobiass.nilsson@sharkmob.com>
2021-12-13 17:45:14 +0100

    Day 10 wip

---
+++ Day10/day10.py
@@ -0,0 +1,77 @@
+inputFile = open("input.txt", "r")
+inputLines = inputFile.readlines()
+
+chunkOpeners = ["(", "[", "{", "<"]
+chunkClosers = [")", "]", "}", ">"]
+
+def ExistsIn(InValue, InArray):
+    for index, value in enumerate(InArray):
+        if value == InValue:
+            return True
+    return False
+
+def GetEquivlant(InCloser):
+    if InCloser == '}':
+        return '{'
+
+    if InCloser == '>':
+        return '<'
+
+    if InCloser == ']':
+        return '['
+
+    if InCloser == ')':
+        return '('
+
+    return ' '
+
+chunkStack = []
+def IsCurrupt(InLine):
+
+
+    for index, char in enumerate(InLine):
+        if ExistsIn(char, chunkOpeners):
+            chunkStack.append(char)
+            continue
+
+        if ExistsIn(char, chunkClosers):
+            if chunkStack[len(chunkStack) - 1] != GetEquivlant(char):
+                print("Currupt " + char + " @ " + str(index))
+                return False
+            else:
+                chunkStack.pop()
+    # Part 1, ignore all non currupted
+
+    return True
+
+scores = []
+for index, line in enumerate(inputLines):
+    print("index: " + str(index))
+    chunkStack = []
+    isCurrupt = IsCurrupt(line) 
+    if isCurrupt:
+        continue
+
+    print(str(chunkStack))
+
+    count = 0
+    for closerIndex, closerValue in enumerate(chunkStack):
+        count *= 5
+        print(str(count))
+        if closerValue == '{':
+            count += 3
+
+        if closerValue == '<':
+            count += 4
+
+        if closerValue == '[':
+            count += 2
+
+        if closerValue == '(':
+            count += 1
+        print(" a "+str(count))
+
+    scores.append(count)
+
+scores.sort()
+print(str(scores))
