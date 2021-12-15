9760e84dc65c71b3732e5c798786aa94205046f8
Tibbd5 <tobias.knislinge@hotmail.com>
2021-12-15 22:27:16 +0100

    working force scan, broken on last test

--- Day15/day15.py
+++ Day15/day15.py
@@ -4,27 +4,58 @@ lines = file.readlines()
 worldSizeY = len(lines)
 worldSizeX = len(lines[0]) - 1
 
-def GetIndexOf(InX, InY):
-    return InY * worldSizeX + InX
-
-nodes = []
-
-def GetNodeOfPos(InX, InY):
-    return nodes[InY][InX]
-
-def GetNodeOf(InIndex):
-    yPos = int(InIndex / worldSizeX)
-    xPos = InIndex - yPos * worldSizeX
-    return nodes[yPos][xPos]
-
 class Node:
     XPos = 0
     YPos = 0
     Index = 0
     ParentRisk = 0
     ParentIndex = -1
-    Distance = 1
     RiskValue = -1
+    bClosed = False
+
+nodes = []
+
+def GetIndexOf(InX, InY):
+    return InY * worldSizeX + InX
+
+# for y in range(worldSizeY):
+#     nodes.append([])
+#     for x in range(worldSizeX):
+#         node = Node()
+#         node.Index = GetIndexOf(x,y)
+#         node.RiskValue = int(lines[y][x])
+#         node.XPos = x
+#         node.YPos = y
+#         nodes[y].append(node)
+
+
+def GetNodeOfPos(InX, InY):
+    return nodes[InY][InX]
+
+# Make the world 5x larger
+
+for yMul in range(5):
+    for y in range(worldSizeY):
+        nodes.append([])
+        for xMul in range(5):
+            for x in range(worldSizeX):
+                node = Node()
+                baseRisk = int(lines[y][x])
+                node.RiskValue = (baseRisk + xMul + yMul - 1) % 9 + 1
+                node.XPos = x + xMul * worldSizeX
+                node.YPos = y + yMul * worldSizeY
+                node.Index = node.YPos * worldSizeX * 5 + node.XPos
+
+                nodes[y + yMul * worldSizeY].append(node)
+
+worldSizeY *= 5
+worldSizeX *= 5
+openNodeIndexes = [ 0 ] 
+
+def GetNodeOf(InIndex):
+    yPos = int(InIndex / worldSizeX)
+    xPos = InIndex - yPos * worldSizeX
+    return nodes[yPos][xPos]
 
 class color:
    PURPLE = '\033[95m'
@@ -39,11 +70,7 @@ class color:
    END = '\033[0m'
 
 def GetTotalRiskOfNode(InNode):
-    endDiffX = worldSizeX - node.XPos
-    endDiffY = worldSizeY - node.YPos
-    diff = endDiffX*endDiffX + endDiffY*endDiffY
     return -(InNode.ParentRisk + InNode.RiskValue)
-    # return -(node.TotalRisk * 2 + node.Distance + diff / (worldSizeX * worldSizeX))
 
 def GetTotalRiskOf(InIndex):
     return GetTotalRiskOfNode(GetNodeOf(InIndex))
@@ -74,25 +101,16 @@ def PrintNodePath(InNode):
                 else:
                     direction = "-"
 
-            string = direction #+ " " + str(node.RiskValue) # + " " + str(node.ParentRisk + node.RiskValue)+ " " + str(node.Distance) + " " + str(int(GetTotalRiskOf(node.Index) + 1))
+            # string = direction #+ " " + str(node.RiskValue) # + " " + str(node.ParentRisk + node.RiskValue)+ " " + str(node.Distance) + " " + str(int(GetTotalRiskOf(node.Index) + 1))
+            string = str(node.RiskValue)#+ " " + str(node.Distance) + " " + str(int(GetTotalRiskOf(node.Index) + 1))
+
             if finalPathIndexes.__contains__(GetIndexOf(x,y)):
                 print('\033[38;5;' + str(15 + node.RiskValue * 2) + "m" + str(string) + color.END, end="")
             else:
                 print('\033[38;5;' + str(231 + node.RiskValue * 2) + "m" + str(string) + color.END, end="")
         print("")
     
-for y in range(worldSizeY):
-    nodes.append([])
-    for x in range(worldSizeX):
-        node = Node()
-        node.Index = GetIndexOf(x,y)
-        node.RiskValue = int(lines[y][x])
-        node.XPos = x
-        node.YPos = y
-        nodes[y].append(node)
 
-openNodeIndexes = [ 0 ] 
-closedIndexes = []
 
 def TryAddPosition(InX, InY, InParentNode):
     if InX < 0:
@@ -108,55 +126,55 @@ def TryAddPosition(InX, InY, InParentNode):
     if node.Index == 0:
         return
 
-    if not closedIndexes.__contains__(GetIndexOf(InX, InY)):
+    if not node.bClosed:
         openNodeIndexes.append(node.Index)
 
-    totalParentRisk = abs(GetTotalRiskOfNode(InParentNode))
-
     if node.ParentIndex == -1:
-        node.ParentRisk = totalParentRisk
+        node.ParentRisk = InParentNode.ParentRisk + InParentNode.RiskValue
         node.ParentIndex = InParentNode.Index
-        node.Distance = InParentNode.Distance + 1
-        # print("Set value of " + str(node.XPos) + " " + str(node.YPos))
     else:
         # We have not evaluated the node previusly, so add it to the 
-        if (totalParentRisk + node.RiskValue) < GetTotalRiskOfNode(node):
-            node.ParentRisk = totalParentRisk
+        if (InParentNode.ParentRisk + InParentNode.RiskValue) <= node.ParentRisk :
+            node.ParentRisk = InParentNode.ParentRisk + InParentNode.RiskValue
             node.ParentIndex = InParentNode.Index
-            node.Distance = InParentNode.Distance + 1
-
 
     
-
+stepCount = 0
 while len(openNodeIndexes) > 0:
+    ## Get the lowest risk node in the open list
     # Sort based on total risk, evaluate least first
-    openNodeIndexes.sort(key=GetTotalRiskOf)
-    # for i ,index in enumerate(openNodeIndexes):
-    #     print("SortedValues " + str(GetTotalRiskOf(index)))
-## TODO: Validate that this is sorts right direction
-    currentNodeIndex = openNodeIndexes.pop()
-    if closedIndexes.__contains__(currentNodeIndex):
-        continue
-
+    # openNodeIndexes.sort(key=GetTotalRiskOf)
+    currentNodeIndex = openNodeIndexes.pop(0)
     currentNode = GetNodeOf(currentNodeIndex)
-    PrintNodePath(currentNode)
-    print("")
-    print("")
-    closedIndexes.append(GetIndexOf(currentNode.XPos, currentNode.YPos))
-    # print("Lowest risk: " + str(currentNode.RiskValue) + " Total: " + str(GetTotalRiskOf(currentNode.Index)))
+
+    # Make shure that we dont doubble scan
+    if currentNode.bClosed:
+        continue
+    stepCount += 1
+
+    currentNode.bClosed = True
+    # add it to the closed list
+    
     x = currentNode.XPos
     y = currentNode.YPos
 
+    if stepCount % 1000 == 0:
+    #     PrintNodePath(currentNode)
+        print(str(stepCount))
+
+    # If we have reached the end, break
     if currentNode.XPos == worldSizeX - 1:
         if currentNode.YPos == worldSizeY - 1:
             break
 
+    # try all positions
     TryAddPosition(x + 1, y, currentNode)
     TryAddPosition(x - 1, y, currentNode)
 
     TryAddPosition(x, y + 1, currentNode)
     TryAddPosition(x, y - 1, currentNode)
 
-PrintNodePath(currentNode)
+# PrintNodePath(currentNode)
 
-print("Donw")
+print(str(abs(currentNode.ParentRisk + currentNode.RiskValue) - nodes[0][0].RiskValue))
+print("In " + str(stepCount) + " steps")
